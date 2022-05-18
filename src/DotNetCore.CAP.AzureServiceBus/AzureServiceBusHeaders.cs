34e495c0ebdb662bb3256dd103485f8c2c8462fe
Mateus Viegas <mateuscviegas@gmail.com>
2022-05-18 09:26:46 +0100

    Moved Destination header to AzureServiceBusHeaders.cs and updated refs

--- src/DotNetCore.CAP.AzureServiceBus/AzureServiceBusHeaders.cs
+++ src/DotNetCore.CAP.AzureServiceBus/AzureServiceBusHeaders.cs
@@ -3,5 +3,10 @@ namespace DotNetCore.CAP.AzureServiceBus
     public static class AzureServiceBusHeaders
     {
         public const string SessionId = "cap-session-id";
+
+        /// <summary>
+        /// Specifies a destination Topic where the Message will be sent to, if there are Multiple topics configured.
+        /// </summary>
+        public const string DestinationTopicPath = "cap-destination-topic-path";
     }
 }
